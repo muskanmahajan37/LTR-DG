796e7195615f79b80c8765a1c66bc477d1f85c61
SHUVASHISH <shuvashish.chatterjee@gmail.com>
2019-10-12 13:22:01 +0530

    pretrained

--- train_distractor.sh
+++ train_distractor.sh
@@ -7,3 +7,4 @@ CUDA_VISIBLE_DEVICES=3 python gan.py --dataset ./data/mcql_processed/ \
   --max_sequence_length_q 80\
   --max_sequence_length_a 20\
   --pretrained_embeddings_path ./data_embeddings/glove.840B.300d.txt\
+  --pretrained_model_path ./model/ \
